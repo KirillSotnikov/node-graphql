c8109dd9179a1a15d6d0cbc633c8ffea4d75cf2b
kirill <kirill@Mac-mini-kirill.local>
2019-12-19 16:45:14 +0200

    init project

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const graphqlHTTP = require('express-graphql')
+const seqelize = require('./utils/database')
+const schema  = require('./graphql/schema')
+const resolver  = require('./graphql/resolver')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+
+app.use(graphqlHTTP({
+  schema: schema,
+  rootValue: resolver
+}))
+
+app.use((req, res, next) => {
+  res.sendFile('/index.html')
+})
+
+async function start() {
+  try{
+    await seqelize.sync({
+      // force: true
+    })
+
+    app.listen(PORT)
+  } catch( err ){
+    console.log(err)
+  }
+}
+
+start()
